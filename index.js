ae2fc1ae0f9bb725df43807da849d8adf6d9da28
Marni1 <m.wawrzczakk@gmail.com>
2024-04-14 16:01:19 +0200

    Added renderAccessories and calculate fn

--- index.js
+++ index.js
@@ -6,14 +6,46 @@ const $navBuyForm = document.querySelector(".nav__buyform");
 const $navHomepage = document.querySelector(".nav__homepage");
 const $payRadios = document.querySelectorAll("input[name=pay]");
 const $nameField = document.querySelector("#name");
+const $deliverySelect = document.querySelector("#delivery");
 const $form = document.querySelector(".buypage__form");
+const $selectedCarDetails = document.querySelector(".buypage__selectedcar");
+const $accessoriesList = document.querySelector(".accessories");
+const $summaryCart = document.querySelector(".summary__cart");
 
 let CARS = [];
 let SELECTEDCAR = null;
+let CART = [];
 const FORMFIELDS = {
   payment: "gotówka",
   name: "",
   date: null,
+};
+MAXDATE = 14;
+
+const calculateTotalPRICE = () => {
+  const cartCost = CART.reduce((acc, item) => {
+    return acc + Number(item.price);
+  }, 0);
+  console.log(cartCost);
+};
+const generateDate = (daysaHead = 0) => {
+  const date = new Date();
+  const newDate = new Date(date);
+  newDate.setDate(date.getDate() + daysaHead);
+  const convertedNewDate = newDate.toLocaleDateString();
+  return convertedNewDate;
+};
+const renderDateOptions = () => {
+  for (let i = 1; i <= MAXDATE; i++) {
+    let option = document.createElement("option");
+    const date = generateDate(i);
+    option.value = date;
+    option.innerText = date;
+    if (i === 1) {
+      FORMFIELDS.date = date;
+    }
+    $deliverySelect.appendChild(option);
+  }
 };
 
 const handleCarPaymentOption = (e) => {
@@ -24,7 +56,6 @@ const handleNameChange = (e) => {
   FORMFIELDS.name = e.target.value;
   console.log(FORMFIELDS);
 };
-console.log($payRadios);
 const changePages = () => {
   $homepage.classList.toggle("active");
   $navHomepage.classList.toggle("active");
@@ -33,20 +64,35 @@ const changePages = () => {
 };
 
 const selectCar = (car) => {
-  selectedCar = car;
-  //render na innej stronie
+  SELECTEDCAR = car;
+
+  renderCarDetails(car);
+  renderCarAccesories(car);
 
   //chowanie strony
   changePages();
 };
-
+const renderCarDetails = (car) => {
+  $selectedCarDetails.innerHTML = `
+  <img class="selectedcar__img" />
+  <p class="selectedcar__brand">${car.brand}</p>
+  <p class="selectedcar__model">${car.model}</p>
+  <p class="selectedcar__year">${car.year}</p>
+  <p class="selectedcar__power">${car.power}</p>
+  <p class="selected__mileage">${car.mileage}</p>
+  
+  `;
+};
 const renderCar = (car) => {
   const carCard = document.createElement("div");
   carCard.classList.add("offer__car");
   carCard.innerHTML = ` <img src=${car.photo} alt="" class="car__img" />
-    <h3 class="car__model">${car.name}</h3>
-    <p class="car__brand">${car.brand}</p>
-    <p class="car__price">${car.price}</p>`;
+  <p class="car__brand">${car.brand}</p>
+    <p class="car__model">${car.model}</h3>
+    <p class="car__year">Rok: ${car.year}</p>
+    <p class="car__millage">Przebieg: ${car.mileage}</p>
+    <p class="car__engine">Moc: ${car.power}</p>
+    <p class="car__price">Cena: ${car.price}</p>`;
   const selectButton = document.createElement("button");
   selectButton.innerText = "Sprawdź oferte";
   selectButton.className = "car__select";
@@ -56,7 +102,71 @@ const renderCar = (car) => {
   carCard.appendChild(selectButton);
   $offersList.appendChild(carCard);
 };
+const renderCarAccesories = (car) => {
+  $accessoriesList.innerHTML = "";
+  const accesories = car.accessories.filter((accessorie) => {
+    return !CART.find((item) => {
+      return item.id === accessorie.id;
+    });
+  });
+  if (accesories.length === 0) {
+    const info = document.createElement("li");
+    info.innerText = "Samochód nie posiada więcej dodatków";
+    $accessoriesList.appendChild(info);
+    return;
+  }
+  accesories.forEach((accesorie) => renderAcessorie(accesorie, car));
+};
+
+const renderAcessorie = (accessorie, car) => {
+  const item = document.createElement("li");
+  const price = document.createElement("p");
+  const name = document.createElement("p");
+  const button = document.createElement("button");
+  button.innerText = "+";
+  price.innerText = accessorie.name;
+  name.innerText = accessorie.price;
+  button.addEventListener("click", () => {
+    const newCart = CART.concat(accessorie);
+    CART = newCart;
+    renderCarAccesories(car);
+    renderCart();
+    calculateTotalPRICE();
+  });
+  item.append(price, name, button);
+  $accessoriesList.appendChild(item);
+};
 const renderCars = (carsList, filter) => {};
+const renderCart = () => {
+  $summaryCart.innerHTML = "";
+  if (CART.length === 0) {
+    const info = document.createElement("li");
+    info.innerText = "Brak wybranych akcesoriów";
+    $summaryCart.appendChild(info);
+    return;
+  }
+  CART.forEach((item) => {
+    const cartItem = document.createElement("li");
+    cartItem.innerHTML = `
+    <p>${item.name}</p>
+    <p>${item.price}</p>
+    `;
+    const button = document.createElement("button");
+    button.innerText = "-";
+    cartItem.appendChild(button);
+    button.addEventListener("click", () => {
+      const cartAfterRemove = CART.filter((cartItem) => {
+        return item.id !== cartItem.id;
+      });
+      CART = cartAfterRemove;
+      renderCarAccesories(SELECTEDCAR);
+      renderCart();
+      calculateTotalPRICE();
+    });
+
+    $summaryCart.appendChild(cartItem);
+  });
+};
 
 const getCars = async () => {
   const resp = await fetch("./cars.json");
@@ -64,8 +174,8 @@ const getCars = async () => {
   CARS = data;
   CARS.forEach(renderCar);
 };
-
 getCars();
+renderDateOptions();
 
 $goBackBtn.addEventListener("click", changePages);
 $payRadios.forEach((radio) => {
